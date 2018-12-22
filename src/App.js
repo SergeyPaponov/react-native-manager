9fe7976f2c87388f3d3d9a75f4e7da6ba8bb11c5
Sergey A. Paponov <sergey.pap@gmail.com>
2018-12-22 16:35:21 +0200

    001

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+import Router from './Router';
+
+class App extends Component {
+  componentWillMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: "AIzaSyAKx91j59MYQAZDNFXsc-9tH_fFPLBT4b4",
+      authDomain: "manager-f9161.firebaseapp.com",
+      databaseURL: "https://manager-f9161.firebaseio.com",
+      projectId: "manager-f9161",
+      storageBucket: "manager-f9161.appspot.com",
+      messagingSenderId: "239351781482"
+    };
+    firebase.initializeApp(config);
+  }
+  render(){
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    return (
+      <Provider store={store}>
+        <Router />
+      </Provider>
+    );
+  }
+}
+
+export default App;
